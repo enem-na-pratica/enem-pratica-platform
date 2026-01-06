2036e9c4c7e8f3cd103e06410227bea1ad6d97d7
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-06 18:11:35 -0300

    refactor(user): rename makeGetUser to makeUserService

--- src/ui/application/fatories/user/user-service.factory.ts
+++ src/ui/application/fatories/user/user-service.factory.ts
@@ -1,7 +1,7 @@
-import { UserService } from "@/src/ui/application/services/infrastructure/api/get-user";
+import { UserService } from "@/src/ui/application/services/infrastructure/api/user-servive";
 import { UserMapper } from "@/src/ui/application/services/infrastructure/mappers/user.mapper";
 
-export function makeGetUser() {
+export function makeUserService() {
   return new UserService({
     mapper: new UserMapper()
   });
