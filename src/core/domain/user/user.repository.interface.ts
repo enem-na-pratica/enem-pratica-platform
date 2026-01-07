7bf03860d521e6abb3678c32a4c5b8f2045cfe44
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-07 18:47:44 -0300

    feat: implement userRepository findAll method to retrieve all users

--- src/core/domain/user/user.repository.interface.ts
+++ src/core/domain/user/user.repository.interface.ts
@@ -1,5 +1,6 @@
 import { User } from "@/src/core/domain/user/user.entity";
 
 export interface UserRepository {
+  findAll(): Promise<User[]>;
   findByUsername(username: string): Promise<User | null>;
 }
