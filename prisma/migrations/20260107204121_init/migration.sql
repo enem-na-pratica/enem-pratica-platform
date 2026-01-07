873d4cf9d9b50e6da06de5e3dfc34fa1f51c92fd
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-07 18:02:28 -0300

    chore: reset migrations and rename superadmin to super_admin

--- prisma/migrations/20260107204121_init/migration.sql
+++ prisma/migrations/20260107204121_init/migration.sql
@@ -1,3 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('STUDENT', 'TEACHER', 'ADMIN', 'SUPER_ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "student_teachers" (
     "id" TEXT NOT NULL,
@@ -7,6 +23,12 @@ CREATE TABLE "student_teachers" (
 
     CONSTRAINT "student_teachers_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE INDEX "users_username_idx" ON "users"("username");
 
 -- AddForeignKey
 ALTER TABLE "student_teachers" ADD CONSTRAINT "student_teachers_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
