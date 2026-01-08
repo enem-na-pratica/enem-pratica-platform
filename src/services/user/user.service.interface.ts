acf9a027864808b092c30dd62dad0d130cd2ed91
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-08 20:38:01 -0300

    feat: implement login API service

--- src/services/user/user.service.interface.ts
+++ src/services/user/user.service.interface.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "@/src/services/models";
 
 export interface UserServiceHttp {
+  login(params: { username: string, password: string }): Promise<void>;
   getMe(): Promise<UserModel>;
 }
