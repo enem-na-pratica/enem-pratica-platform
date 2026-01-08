f64314f4e0580d522398d481d8a54d04afca8de1
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-08 10:20:55 -0300

    feat: add route to retrieve the full users list

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,16 @@
+import { Role } from "@/src/core/domain/auth/roles";
+import { makeListUsersController } from "@/src/core/main/factories/user/list-users.factory";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const userRole = request.headers.get("x-user-role") as Role;
+
+  const listUsersController = await makeListUsersController().handle({
+    body: userRole
+  });
+
+  return NextResponse.json(
+    listUsersController.body,
+    { status: listUsersController.statusCode }
+  );
+}
