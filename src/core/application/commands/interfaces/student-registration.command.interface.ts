308949270e75ca05538978b329434b327d3bdfcf
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-09 16:27:52 -0300

    feat: add create method to UserPrismaRepository

--- src/core/application/commands/interfaces/student-registration.command.interface.ts
+++ src/core/application/commands/interfaces/student-registration.command.interface.ts
@@ -1,5 +1,5 @@
 import { User } from "@/src/core/domain/user/user.entity";
 
 export interface StudentRegistrationCommand {
-  registerStudent(params: {student: User, teacherId: string}): Promise<void>;
+  registerStudent(params: {student: User, teacherId: string}): Promise<User>;
 }
