5c357aa5ef2247b377994551e27a4fa18bb11a7f
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-09 13:05:57 -0300

    feat: add interfaces for new student creation

---
+++ src/core/application/dtos/user/create-user.dto.ts
@@ -0,0 +1,6 @@
+export type CreateUserDto = {
+  name: string;
+  username: string;
+  password: string;
+  role: string;
+}
--- src/core/application/dtos/user/index.ts
+++ src/core/application/dtos/user/index.ts
@@ -1,2 +1,4 @@
+export * from "./create-student.dto";
+export * from "./create-user.dto";
 export * from "./user.dto";
 export * from "./teaching-staff-option.dto";
