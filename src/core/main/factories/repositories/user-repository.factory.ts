d67be595bff6a55957f043f52715a22692e1eb13
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-11 15:08:22 -0300

    refactor: rename UserPrismaMapper to PrismaUserMapper and update references

--- src/core/main/factories/repositories/user-repository.factory.ts
+++ src/core/main/factories/repositories/user-repository.factory.ts
@@ -1,10 +1,10 @@
 import { UserPrismaRepository } from "@/src/core/infrastructure/repositories/prisma/user-prisma.repository";
 import { prisma } from '@/src/core/infrastructure/databases/prisma/prisma';
-import { UserPrismaMapper } from '@/src/core/infrastructure/mapper/user-prisma.mapper';
+import { PrismaUserMapper } from '@/src/core/infrastructure/mapper/prisma-user.mapper';
 import { UserRepository } from "@/src/core/domain/user/user.repository.interface";
 
 export function makeUserPrismaRepository(): UserRepository {
-  const userPrismaMapper = new UserPrismaMapper();
+  const userPrismaMapper = new PrismaUserMapper();
   return new UserPrismaRepository({
     prisma,
     mapper: userPrismaMapper
