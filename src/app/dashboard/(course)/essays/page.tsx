211f40ba25d3824a500b45adb9fabd4ce3719bfa
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-12 16:19:32 -0300

    chore: remove unnecessary icon

--- src/app/dashboard/(course)/essays/page.tsx
+++ src/app/dashboard/(course)/essays/page.tsx
@@ -306,23 +306,6 @@ export default function EssayPage() {
                         essay.competency5}
                     </span>
                   </div>
-
-                  {/* Botão de ação opcional ou apenas um ícone de "chevron" */}
-                  <div className="opacity-20 hover:opacity-100 transition-opacity cursor-pointer">
-                    <svg
-                      className="w-5 h-5"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9 5l7 7-7 7"
-                      />
-                    </svg>
-                  </div>
                 </div>
               </div>
             ))
