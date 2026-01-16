4088c10eccbc603fb03576a95c39ee0ec844d55f
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-15 21:43:58 -0300

    feat: add mapper to convert Essay entity to response DTO

---
+++ src/core/application/mapper/essay-dto.mapper.ts
@@ -0,0 +1,26 @@
+import { ToDtoMapper } from "@/src/core/domain/mapper"
+import { Essay } from "@/src/core/domain/essay/essay.entity";
+import { EssayResDto } from "@/src/core/application/dtos/essay";
+
+export class EssayResDtoMapper implements ToDtoMapper<Essay, EssayResDto> {
+  toDto(essay: Essay): EssayResDto {
+    if (!essay.id) {
+      throw new Error("Cannot map an essay without a valid ID to DTO.");
+    }
+
+    return {
+      id: essay.id,
+      authorId: essay.authorId,
+      theme: essay.theme,
+      grades: {
+        c1: essay.competency1,
+        c2: essay.competency2,
+        c3: essay.competency3,
+        c4: essay.competency4,
+        c5: essay.competency5,
+        total: essay.totalScore,
+      },
+      createdAt: essay.createdAt.toISOString(),
+    };
+  }
+}
--- src/core/application/mapper/user-dto.mapper.ts
+++ src/core/application/mapper/user-dto.mapper.ts
@@ -3,14 +3,18 @@ import { User } from "@/src/core/domain/user/user.entity";
 import { UserResDto } from "@/src/core/application/dtos/user";
 
 export class UserResDtoMapper implements ToDtoMapper<User, UserResDto> {
-  toDto(input: User): UserResDto {
+  toDto(user: User): UserResDto {
+    if (!user.id) {
+      throw new Error("Cannot map an user without a valid ID to DTO.");
+    }
+
     return {
-      id: input.id!,
-      name: input.name,
-      username: input.username,
-      role: input.role,
-      createdAt: input.createdAt,
-      updatedAt: input.updatedAt
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
     } as UserResDto
   }
 }
