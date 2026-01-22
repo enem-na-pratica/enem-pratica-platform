64c75988ef51141ef7d2b5bf8c8bb7c9d6fa83eb
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-22 20:01:38 -0300

    refactor: add index files for create-essay and list-user-essays-summary use cases

---
+++ src/core/application/use-cases-new/essay/create-essay/index.ts
@@ -0,0 +1,2 @@
+export * from './create-essay.dto';
+export * from './create-essay.use-case';
---
+++ src/core/application/use-cases-new/essay/list-user-essays-summary/index.ts
@@ -0,0 +1,3 @@
+export * from './list-essays-by-author.query';
+export * from './list-user-essays-summary.use-case';
+export * from './user-essays-overview.dto';
