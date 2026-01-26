e473b0ca1b8eb66c18253b7f24ba3c8615c583df
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-26 09:11:00 -0300

    feat: create PrismaUserRepository factory

---
+++ src/core/main/factories/new-repositories/make-prisma-user-repository.factory.ts
@@ -0,0 +1,11 @@
+import { PrismaUserRepository } from '@/src/core/infrastructure/databases/prisma/repositories';
+import { UserEntityMapper } from '@/src/core/infrastructure/databases/prisma/mappers';
+import { prisma } from '@/src/core/infrastructure/databases/prisma/prisma'
+
+export function makePrismaUserRepository() {
+  const userEntityMapper = new UserEntityMapper();
+  return new PrismaUserRepository({
+    prisma,
+    mapper: userEntityMapper
+  });
+}
