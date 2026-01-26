c8f06984ab3a261254b31cc74181f43b519db260
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-26 12:51:32 -0300

    feat(auth): create dedicated JwtAdapter factory for reuse across the application

--- src/core/main/factories/auth/make-login-controller.factory.ts
+++ src/core/main/factories/auth/make-login-controller.factory.ts
@@ -1,17 +1,15 @@
 import { LoginController } from "@/src/core/presentation/controllers/auth";
 import { LoginUseCase } from "@/src/core/application/use-cases/auth";
 import { makePrismaUserRepository } from "@/src/core/main/factories/common/repositories";
-import { JwtAdapter } from "@/src/core/infrastructure/auth";
 import { BcryptAdapter } from "@/src/core/infrastructure/crypto";
 import { ZodValidator, loginSchema } from "@/src/core/infrastructure/validation/zod";
-import env from "@/src/core/main/config/env";
+import { makeJwtAdapter } from "@/src/core/main/factories/common/auth";
 
 export function makeLoginController() {
-  const jwtAdapter = new JwtAdapter({ secret: env.jwtSecret, expiresIn: '7D' });
   const bcryptAdapter = new BcryptAdapter();
   const loginUseCase = new LoginUseCase({
     userRepository: makePrismaUserRepository(),
-    tokenGenerator: jwtAdapter,
+    tokenGenerator: makeJwtAdapter(),
     hashComparer: bcryptAdapter
   });
 
