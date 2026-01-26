79c8eba3b45c8de9b2b43ca12ad84b75775e079b
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-26 13:32:58 -0300

    feat(user): create UserDtoMapper factory

---
+++ src/core/main/factories/common/mappers/entity-to-dto/index.ts
@@ -0,0 +1,1 @@
+export * from './make-user-dto.mapper';
--- src/core/main/factories/new-user/make-create-user-controller.factory.ts
+++ src/core/main/factories/new-user/make-create-user-controller.factory.ts
@@ -1,18 +1,17 @@
 import { CreateUserController } from '@/src/core/presentation/controllers/user';
 import { CreateUserUseCase } from '@/src/core/application/use-cases/user';
-import { UserDtoMapper } from '@/src/core/application/mapper';
 import { makePrismaUserRepository } from '@/src/core/main/factories/common/repositories';
 import {
   ZodValidator,
   createUserSchema
 } from '@/src/core/infrastructure/validation/zod';
 import { makeBcryptAdapter } from "@/src/core/main/factories/common/crypto";
+import { makeUserDtoMapper } from "@/src/core/main/factories/common/mappers/entity-to-dto";
 
 export function makeCreateUserController() {
-  const userDtoMapper = new UserDtoMapper();
   const createUserUseCase = new CreateUserUseCase({
     hasher: makeBcryptAdapter(),
-    mapper: userDtoMapper,
+    mapper: makeUserDtoMapper(),
     userRepository: makePrismaUserRepository(),
   });
 
