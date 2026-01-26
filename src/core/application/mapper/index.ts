2a620a6a745253ecfa9c11f3fed6bd21ef67aa8a
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-26 10:36:03 -0300

    feat: create create essay controller factory

---
+++ src/core/application/mapper/index.ts
@@ -0,0 +1,2 @@
+export * from './essay-dto.mapper';
+export * from './user-dto.mapper';
---
+++ src/core/main/factories/new-essay/make-create-essay-controller.factory.ts
@@ -0,0 +1,26 @@
+import { CreateEssayController } from "@/src/core/presentation/controllers/essay";
+import { CreateEssayUseCase } from "@/src/core/application/use-cases/essay";
+import {
+  makePrismaUserRepository,
+  makePrismaEssayRepository,
+  makePrismaStudentTeacherRepository
+} from "@/src/core/main/factories/repositories";
+import { EssayDtoMapper } from "@/src/core/application/mapper";
+import { ZodValidator, createEssaySchema } from "@/src/core/infrastructure/validation/zod";
+
+export function makeCreateEssayController() {
+  const essayDtoMapper = new EssayDtoMapper();
+  const createEssayUseCase = new CreateEssayUseCase({
+    essayRepository: makePrismaEssayRepository(),
+    userRepository: makePrismaUserRepository(),
+    studentTeacherRepository: makePrismaStudentTeacherRepository(),
+    mapper: essayDtoMapper,
+  });
+
+  const zodValidator = new ZodValidator(createEssaySchema);
+
+  return new CreateEssayController({
+    createEssayUseCase,
+    validator: zodValidator,
+  });
+}
