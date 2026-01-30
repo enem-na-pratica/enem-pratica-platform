ef3686aba1462664a4e40b5e679e771889eb4c05
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-30 19:25:52 -0300

    refactor(auth): update logout route to use custom adapter

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,26 +1,5 @@
-import { makeLogoutController } from "@/src/core/main/factories/auth/logout.factory";
-import { NextResponse, NextRequest } from "next/server";
+import { nextRouteAdapter } from '@/src/core/main/adapters';
+import { AuthFactories } from '@/src/core/main/factories';
 
-const AUTH_COOKIE_NAME = "auth_token";
-
-export async function DELETE(_request: NextRequest) {
-  const controllerResponse = await makeLogoutController().handle({
-    body: undefined,
-  });
-
-  if (controllerResponse.statusCode !== 204) {
-    return NextResponse.json(
-      controllerResponse.body,
-      { status: controllerResponse.statusCode }
-    );
-  }
-
-  const response = new NextResponse(
-    controllerResponse.body,
-    { status: controllerResponse.statusCode }
-  );
-
-  response.cookies.delete(AUTH_COOKIE_NAME);
-
-  return response;
-}
+const logout = AuthFactories.makeLogout();
+export const DELETE = nextRouteAdapter(logout);
