528b53fe0a61e8f626968dc4336413f9fef75948
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-30 20:23:21 -0300

    refactor(essay): move create essay route to /api/essays and use adapter

---
+++ src/app/api/essays/route.ts
@@ -0,0 +1,5 @@
+import { nextRouteAdapter } from '@/src/core/main/adapters';
+import { EssayFactories } from '@/src/core/main/factories';
+
+const createEssay = EssayFactories.makeCreateEssay();
+export const POST = nextRouteAdapter(createEssay);
