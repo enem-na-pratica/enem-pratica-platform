fb1e6c619a0f9d29889df69bf0a44e7cc2b82a63
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-01-30 18:47:50 -0300

    refactor(auth): simplify login route handling and structure

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,45 +1,5 @@
-import { makeLoginController } from "@/src/core/main/factories/auth/login.factory";
-import { CookieData } from "@/src/core/presentation/interfaces";
-import { NextResponse, NextRequest } from "next/server";
+import { nextRouteAdapter } from '@/src/core/main/adapters';
+import { AuthFactories } from '@/src/core/main/factories';
 
-
-export async function POST(request: NextRequest) {
-  let credentials;
-
-  try {
-    if (request.headers.get("content-type")?.includes("application/json")) {
-      credentials = await request.json();
-    }
-  } catch {
-    return NextResponse.json(
-      { error: "Invalid or malformed JSON request body." },
-      { status: 400 }
-    );
-  }
-
-  const loginController = await makeLoginController().handle({ body: credentials });
-
-  if (loginController.statusCode !== 204) {
-    return NextResponse.json(
-      loginController.body,
-      { status: loginController.statusCode }
-    );
-  }
-
-  const response = new NextResponse(
-    null,
-    { status: loginController.statusCode }
-  );
-
-  if (loginController.cookies) {
-    loginController.cookies.forEach((cookie: CookieData) => {
-      response.cookies.set(
-        cookie.name,
-        cookie.value,
-        cookie.options
-      );
-    });
-  }
-
-  return response;
-}
+const login = AuthFactories.makeLogin();
+export const POST = nextRouteAdapter(login);
