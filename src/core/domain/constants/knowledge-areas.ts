1a614fca8838d2ea9895d98f4b23ba35ecf7eef2
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-05 08:17:38 -0300

    feat(domain): implement AreaPerformance entity

--- src/core/domain/constants/knowledge-areas.ts
+++
@@ -1,8 +0,0 @@
-export const knowledgeArea = {
-  LANGUAGES: 'LANGUAGES',
-  HUMANITIES: 'HUMANITIES',
-  NATURAL_SCIENCES: 'NATURAL_SCIENCES',
-  MATHEMATICS: 'MATHEMATICS',
-} as const;
-
-export type KnowledgeArea = typeof knowledgeArea[keyof typeof knowledgeArea];
