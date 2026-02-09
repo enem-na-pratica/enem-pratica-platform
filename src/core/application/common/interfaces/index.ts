70e3755329ddbd8f55a19f5fc52c357e6199af45
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-09 18:58:04 -0300

    chore(core): remove unused Requester interface file

--- src/core/application/common/interfaces/index.ts
+++ src/core/application/common/interfaces/index.ts
@@ -1,3 +1,2 @@
 export * from './query.interface';
 export * from './use-case.interface';
-export * from './requester.interface';
--- src/core/application/common/interfaces/requester.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from "@/src/core/domain/auth";
-
-export type Requester = {
-  id: string;
-  username: string;
-  role: Role;
-};
