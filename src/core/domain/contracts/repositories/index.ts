5e2169b3fc7cc2e26f22031c51e21b8b5b96954e
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-23 20:30:56 -0300

    feat(domain): add UserTopicProgressRepository interface

--- src/core/domain/contracts/repositories/index.ts
+++ src/core/domain/contracts/repositories/index.ts
@@ -2,3 +2,4 @@ export * from './essay-repository.interface';
 export * from './student-teacher-repository.interface';
 export * from './user-repository.interface';
 export * from './mock-exam-repository.interface';
+export * from './user-topic-progress-repository.interface';
