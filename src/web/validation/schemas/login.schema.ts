16651913b0e53dd75ad13d73135a7d938f2fd404
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-24 17:20:03 -0300

    refactor(schema): reorganize import statements in loginSchema

--- src/web/validation/schemas/login.schema.ts
+++ src/web/validation/schemas/login.schema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
-import { usernameSchema, passwordSchema } from './common';
+
+import { passwordSchema, usernameSchema } from './common';
 
 export const loginSchema = z.object({
   username: usernameSchema,
 
+  // TODO: replace with passwordSchema.
+  // Using a simplified password validation for mocked credentials during development.
   password: z.string().trim().min(8).max(30),
 });
-
