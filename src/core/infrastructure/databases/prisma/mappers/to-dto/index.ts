3b5bf87e33a851a44edf21ab96f7a792a4a5ec78
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-26 19:58:16 -0300

    feat(infra): add PrismaTopicProgressDtoMapper

--- src/core/infrastructure/databases/prisma/mappers/to-dto/index.ts
+++ src/core/infrastructure/databases/prisma/mappers/to-dto/index.ts
@@ -2,3 +2,4 @@ export * from './prisma-user-dto.mapper';
 export * from './prisma-essay-dto.mapper';
 export * from './prisma-mock-exam-dto.mapper';
 export * from './prisma-subject-dto.mapper';
+export * from './prisma-topic-progress-dto.mapper';
