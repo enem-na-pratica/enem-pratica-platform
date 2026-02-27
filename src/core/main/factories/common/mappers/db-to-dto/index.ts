e3c76b0e7f4c5c75fd72b8d15749ab7ec77a4e31
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-27 09:01:25 -0300

    feat(factory): add makePrismaTopicProgressDtoMapper factory

--- src/core/main/factories/common/mappers/db-to-dto/index.ts
+++ src/core/main/factories/common/mappers/db-to-dto/index.ts
@@ -2,3 +2,4 @@ export * from './make-prisma-essay-dto-mapper.factory';
 export * from './make-prisma-user-dto-mapper.factory';
 export * from './make-prisma-mock-exam-dto-mapper.factory';
 export * from './make-prisma-subject-dto-mapper.factory';
+export * from './make-prisma-topic-progress-dto-mapper.factory';
