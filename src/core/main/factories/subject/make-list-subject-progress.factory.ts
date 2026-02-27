ee5c0b0949d1021c18d166edf57ac46705e78a0c
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-02-27 09:34:06 -0300

    feat(factories): wire ListSubjectProgressUseCase into factory

--- src/core/main/factories/subject/make-list-subject-progress.factory.ts
+++ src/core/main/factories/subject/make-list-subject-progress.factory.ts
@@ -1,6 +1,8 @@
+import { ListSubjectProgressUseCase } from '@/src/core/application/use-cases/subject';
 import { prisma } from '@/src/core/infrastructure/databases/prisma/prisma';
 import { PrismaListSubjectProgressByTargetUserQuery } from '@/src/core/infrastructure/databases/prisma/queries';
 import { makePrismaTopicProgressDtoMapper } from '@/src/core/main/factories/common/mappers/db-to-dto';
+import { makeUserAccessService } from '@/src/core/main/factories/common/services';
 
 export function makeListSubjectProgress() {
   const listSubjectProgressByTargetUserQuery =
@@ -8,6 +10,11 @@ export function makeListSubjectProgress() {
       prisma,
       mapper: makePrismaTopicProgressDtoMapper(),
     });
+
+  const listSubjectProgressUseCase = new ListSubjectProgressUseCase({
+    userAccessService: makeUserAccessService(),
+    listSubjectProgressByTargetUserQuery,
+  });
 
   throw new Error('Not implemented yet');
 }
