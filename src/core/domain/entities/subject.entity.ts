0df98b377120dcd6fd7f9dbf82ad85598784a29f
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-02 10:42:15 -0300

    feat(domain): add slug field to Subject entity

--- src/core/domain/entities/subject.entity.ts
+++ src/core/domain/entities/subject.entity.ts
@@ -1,29 +1,34 @@
-import { SubjectName } from '@/src/core/domain/value-objects'
+import { Slug, SubjectName } from '@/src/core/domain/value-objects';
 
 type SubjectProps = {
   id?: string;
   name: string;
+  slug: string;
   // category?: string;
   createdAt?: Date;
-}
+};
 
-type CreateSubjectProps = Prettify<Omit<SubjectProps, "id" | "createdAt">>;
+type CreateSubjectProps = Prettify<
+  Omit<SubjectProps, 'id' | 'slug' | 'createdAt'>
+>;
 
 type LoadSubjectProps = Prettify<Required<SubjectProps>>;
 
 export class Subject {
   private _id: string | undefined;
   private _name: SubjectName;
+  private _slug: Slug;
   private _createdAt: Date;
 
   private constructor(props: SubjectProps) {
     this._id = props.id;
     this._name = SubjectName.create(props.name);
+    this._slug = Slug.create(props.slug);
     this._createdAt = props.createdAt || new Date();
   }
 
-  public static create(props: CreateSubjectProps): Subject {
-    return new Subject(props);
+  public static create({ name }: CreateSubjectProps): Subject {
+    return new Subject({ name, slug: name });
   }
 
   public static load(props: LoadSubjectProps): Subject {
@@ -31,12 +36,26 @@ export class Subject {
   }
 
   // --- Getters ---
-  public get id(): string | undefined { return this._id }
-  public get name(): string { return this._name.value }
-  public get createdAt(): Date { return this._createdAt }
+  public get id(): string | undefined {
+    return this._id;
+  }
+  public get name(): string {
+    return this._name.value;
+  }
+  public get slug(): string {
+    return this._slug.value;
+  }
+  public get createdAt(): Date {
+    return this._createdAt;
+  }
 
   // --- Mutations ---
+  private changeSlug(newSlug: string): void {
+    this._slug = Slug.create(newSlug);
+  }
+
   public changeName(newName: string): void {
     this._name = SubjectName.create(newName);
+    this.changeSlug(newName);
   }
 }
