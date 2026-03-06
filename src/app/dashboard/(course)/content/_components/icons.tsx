1ee6d063be03b45646f163158430151f8ccecfec
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-06 11:10:49 -0300

    bolt(web): add BackArrow icon component

---
+++ src/app/dashboard/(course)/content/_components/icons.tsx
@@ -0,0 +1,18 @@
+export function BackArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2.5}
+      stroke="currentColor"
+      className="h-6 w-6 transition-colors hover:text-(--accent)"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
+}
