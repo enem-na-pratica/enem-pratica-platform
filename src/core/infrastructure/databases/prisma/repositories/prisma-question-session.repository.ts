8e71182609d8a1f374d6c4acfb4975bc76e8c052
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-11 14:01:32 -0300

    feat: add PrismaQuestionSessionRepository for managing question sessions

---
+++ src/core/infrastructure/databases/prisma/repositories/prisma-question-session.repository.ts
@@ -0,0 +1,37 @@
+import type { Mapper } from '@/src/core/domain/contracts/mappers';
+import type { QuestionSessionRepository } from '@/src/core/domain/contracts/repositories';
+import type { QuestionSession } from '@/src/core/domain/entities';
+import type {
+  PrismaClient,
+  QuestionSession as PrismaQuestionSession,
+} from '@/src/generated/prisma/client';
+
+type PrismaQuestionSessionRepositoryDeps = {
+  prisma: PrismaClient;
+  mapper: Mapper<PrismaQuestionSession, QuestionSession>;
+};
+
+export class PrismaQuestionSessionRepository implements QuestionSessionRepository {
+  private readonly prisma: PrismaClient;
+  private readonly mapper: Mapper<PrismaQuestionSession, QuestionSession>;
+
+  constructor({ prisma, mapper }: PrismaQuestionSessionRepositoryDeps) {
+    this.prisma = prisma;
+    this.mapper = mapper;
+  }
+
+  async create(questionSession: QuestionSession): Promise<QuestionSession> {
+    const newQuestionSession = await this.prisma.questionSession.create({
+      data: {
+        date: questionSession.date,
+        total: questionSession.total,
+        correct: questionSession.correct,
+        isReviewing: questionSession.isReviewing,
+        author: { connect: { id: questionSession.authorId } },
+        topic: { connect: { id: questionSession.topicId } },
+      },
+    });
+
+    return this.mapper.map(newQuestionSession);
+  }
+}
