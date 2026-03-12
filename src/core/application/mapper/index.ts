13d3842f2f46d81fe8dbf857261af5e0f75af98c
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-11 21:55:38 -0300

    feat(mapper): add QuestionSessionDtoMapper for mapping QuestionSession to QuestionSessionDto

--- src/core/application/mapper/index.ts
+++ src/core/application/mapper/index.ts
@@ -2,3 +2,4 @@ export * from './essay-dto.mapper';
 export * from './user-dto.mapper';
 export * from './mock-exam-dto.mapper';
 export * from './user-topic-progress-dto.mapper';
+export * from './question-session-dto.mapper';
