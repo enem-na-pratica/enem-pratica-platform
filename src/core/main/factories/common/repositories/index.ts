615a1c3ecbdaa1c08598a921acfc0c4fa70a6887
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-11 21:59:29 -0300

    feat(factory): add factory for PrismaQuestionSessionRepository

--- src/core/main/factories/common/repositories/index.ts
+++ src/core/main/factories/common/repositories/index.ts
@@ -3,3 +3,4 @@ export * from './make-prisma-student-teacher-repository.factory';
 export * from './make-prisma-user-repository.factory';
 export * from './make-prisma-mock-exam-repository.factory';
 export * from './make-prisma-user-topic-progress-repository.factory';
+export * from './make-prisma-question-session-repository.factory';
