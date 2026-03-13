fa63a17c27c102decf35ba3698de109f18e0864d
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-13 14:02:39 -0300

    feat(infra): implement getById method in PrismaQuestionSessionRepository

--- src/core/infrastructure/databases/prisma/repositories/prisma-question-session.repository.ts
+++ src/core/infrastructure/databases/prisma/repositories/prisma-question-session.repository.ts
@@ -1,6 +1,7 @@
 import type { Mapper } from '@/src/core/domain/contracts/mappers';
 import type { QuestionSessionRepository } from '@/src/core/domain/contracts/repositories';
 import type { QuestionSession } from '@/src/core/domain/entities';
+import { NotFoundError } from '@/src/core/domain/errors';
 import type {
   PrismaClient,
   QuestionSession as PrismaQuestionSession,
@@ -35,8 +36,19 @@ export class PrismaQuestionSessionRepository implements QuestionSessionRepositor
     return this.mapper.map(newQuestionSession);
   }
 
-  getById(questionSessionId: string): Promise<QuestionSession> {
-    throw new Error('Method not implemented.');
+  async getById(questionSessionId: string): Promise<QuestionSession> {
+    const questionSession = await this.prisma.questionSession.findUnique({
+      where: { id: questionSessionId },
+    });
+
+    if (!questionSession)
+      throw new NotFoundError({
+        entityName: 'QuestionSession',
+        fieldName: 'id',
+        entityValue: questionSessionId,
+      });
+
+    return this.mapper.map(questionSession);
   }
 
   setIsReviewed({
