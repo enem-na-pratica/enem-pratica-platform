74901a617d40a080dedbdcfd9c5393949183d532
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-16 15:12:28 -0300

    feat(application): add initial structure for ListUserQuestionSessionsStatisticsUseCase

---
+++ src/core/application/use-cases/question-session/list-user-question-session-statistics/list-user-question-sessions-statistics.use-case.ts
@@ -0,0 +1,37 @@
+import type { UseCase } from '@/src/core/application/common/interfaces';
+import type { Requester, UserAccessService } from '@/src/core/domain/services';
+
+import type { ListQuestionSessionsByAuthorQuery } from './list-question-session-by-author.query';
+import type { UserQuestionSessionsOverviewDto } from './user-question-session-overview.dto';
+
+type ListUserQuestionSessionsStatisticsUseCaseDeps = {
+  userAccessService: UserAccessService;
+  listQuestionSessionsByAuthorQuery: ListQuestionSessionsByAuthorQuery;
+};
+
+export type ListUserQuestionSessionsStatisticsInput = {
+  authorUsername?: string;
+  requester: Requester;
+};
+
+export class ListUserQuestionSessionsStatisticsUseCase implements UseCase<
+  ListUserQuestionSessionsStatisticsInput,
+  UserQuestionSessionsOverviewDto
+> {
+  private readonly userAccessService: UserAccessService;
+  private readonly listQuestionSessionsByAuthorQuery: ListQuestionSessionsByAuthorQuery;
+
+  constructor({
+    userAccessService,
+    listQuestionSessionsByAuthorQuery,
+  }: ListUserQuestionSessionsStatisticsUseCaseDeps) {
+    this.userAccessService = userAccessService;
+    this.listQuestionSessionsByAuthorQuery = listQuestionSessionsByAuthorQuery;
+  }
+
+  async execute(
+    input: ListUserQuestionSessionsStatisticsInput,
+  ): Promise<UserQuestionSessionsOverviewDto> {
+    throw new Error('Method not implemented.');
+  }
+}
