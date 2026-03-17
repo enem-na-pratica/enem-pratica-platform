cdfa325d0409776de3f372c4a997730851ae2a79
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-17 13:13:11 -0300

    feat(main): add GET route for user question sessions statistics

---
+++ src/app/api/question-session/users/[username]/route.ts
@@ -0,0 +1,6 @@
+import { nextRouteAdapter } from '@/src/core/main/adapters';
+import { QuestionSessionFactories } from '@/src/core/main/factories';
+
+const listUserQuestionSessionsStatistics =
+  QuestionSessionFactories.makeListUserQuestionSessionsStatistics();
+export const GET = nextRouteAdapter(listUserQuestionSessionsStatistics);
