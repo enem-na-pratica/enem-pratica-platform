47931590a19b0a147c1545355d33d282128e226e
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-16 21:26:15 -0300

    feat(infra): implement mapper for QuestionSession with Topic and Subject

--- src/core/infrastructure/databases/prisma/mappers/to-dto/index.ts
+++ src/core/infrastructure/databases/prisma/mappers/to-dto/index.ts
@@ -3,3 +3,4 @@ export * from './prisma-essay-dto.mapper';
 export * from './prisma-mock-exam-dto.mapper';
 export * from './prisma-subject-dto.mapper';
 export * from './prisma-topic-progress-dto.mapper';
+export * from './prisma-question-session-with-topic-subject-dto.mapper';
