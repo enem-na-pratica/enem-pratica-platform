d9c76f720bb3b4f25592f0469501b565c7937215
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-18 15:15:43 -0300

    feat(web): add statistics and overview mapping for the dashboard

--- src/web/api/modules/question-session/question-session.mapper.ts
+++ src/web/api/modules/question-session/question-session.mapper.ts
@@ -1,5 +1,13 @@
-import { QuestionSessionDto } from './question-session.dto';
-import { QuestionSession } from './question-session.model';
+import {
+  QuestionSessionDto,
+  QuestionSessionWithTopicAndSubjectDto,
+  UserQuestionSessionsOverviewDto,
+} from './question-session.dto';
+import {
+  QuestionSession,
+  QuestionSessionWithTopicAndSubject,
+  UserQuestionSessionsOverview,
+} from './question-session.model';
 
 export const QuestionSessionMapper = {
   toModel(dto: QuestionSessionDto): QuestionSession {
@@ -9,6 +17,33 @@ export const QuestionSessionMapper = {
       nextReviewDate: dto.nextReviewDate ? new Date(dto.nextReviewDate) : null,
       createdAt: new Date(dto.createdAt),
       updatedAt: new Date(dto.updatedAt),
+    };
+  },
+
+  mapQuestionSessionDtoToModel(
+    dto: QuestionSessionWithTopicAndSubjectDto,
+  ): QuestionSessionWithTopicAndSubject {
+    return {
+      ...QuestionSessionMapper.toModel(dto),
+      topic: {
+        ...dto.topic,
+        createdAt: new Date(dto.createdAt),
+        subject: {
+          ...dto.topic.subject,
+          createdAt: new Date(dto.topic.subject.createdAt),
+        },
+      },
+    };
+  },
+
+  toOverviewModel(
+    dto: UserQuestionSessionsOverviewDto,
+  ): UserQuestionSessionsOverview {
+    return {
+      ...dto,
+      questionSessions: dto.questionSessions.map((s) =>
+        this.mapQuestionSessionDtoToModel(s),
+      ),
     };
   },
 };
