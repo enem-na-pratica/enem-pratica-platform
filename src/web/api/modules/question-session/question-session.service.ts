3fa51b5ec857bd3067158233b9dff98673e344dc
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-18 15:49:50 -0300

    feat(web): implement setIsReviewed method in QuestionSessionService

--- src/web/api/modules/question-session/question-session.service.ts
+++ src/web/api/modules/question-session/question-session.service.ts
@@ -17,6 +17,11 @@ type CreateQuestionSessionDto = {
   isReviewed?: boolean;
 };
 
+type SetIsReviewedDto = {
+  questionSessionId: string;
+  isReviewed: boolean;
+};
+
 export class QuestionSessionService {
   private readonly httpClient: HttpClient;
 
@@ -30,6 +35,18 @@ export class QuestionSessionService {
     const data = await this.httpClient.post<QuestionSessionDto>({
       endpoint: '/question-sessions',
       options: { data: dataQuestionSession },
+    });
+
+    return QuestionSessionMapper.toModel(data);
+  }
+
+  async setIsReviewed({
+    isReviewed,
+    questionSessionId,
+  }: SetIsReviewedDto): Promise<QuestionSession> {
+    const data = await this.httpClient.post<QuestionSessionDto>({
+      endpoint: '/question-sessions/:questionSessionId',
+      options: { data: { isReviewed }, params: { questionSessionId } },
     });
 
     return QuestionSessionMapper.toModel(data);
