94e4fa310c98dc60688e1a00f8623b9b34f5058d
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-03-26 20:47:15 -0300

    refactor(auth): environment-aware logout and cookie clearance

--- src/core/main/factories/auth/make-logout.factory.ts
+++ src/core/main/factories/auth/make-logout.factory.ts
@@ -1,5 +1,6 @@
-import { LogoutController } from "@/src/core/presentation/controllers/auth";
+import { env } from '@/src/core/main/config';
+import { LogoutController } from '@/src/core/presentation/controllers/auth';
 
 export function makeLogout() {
-  return new LogoutController();
+  return new LogoutController({ isProduction: env.isProduction });
 }
