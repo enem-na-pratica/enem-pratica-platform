9ff35c91823fbac63a4123078f64293526959000
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-04-29 18:25:11 -0300

    refactor: remove doubtHits column from AreaPerformance model and related migration

---
+++ prisma/migrations/20260429211702_remove_doubt_hits_from_area_performance/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `doubtHits` on the `area_performances` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "area_performances" DROP COLUMN "doubtHits";
