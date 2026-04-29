ba941d9988e6b15fe78d0c4d275acfaed8de183c
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2026-04-29 19:25:31 -0300

    refactor: update knowledgeGaps to knowledgeGapsErrors in MockExamItem component

--- src/app/dashboard/(course)/mock-exams/[username]/_components/mock-exam-item.tsx
+++ src/app/dashboard/(course)/mock-exams/[username]/_components/mock-exam-item.tsx
@@ -36,7 +36,7 @@ export function MockExamItem({ mock }: { mock: MockExam }) {
       acc.totalCritical += stats.qualityAssessment.criticalErrors;
       acc.totalDistraction += stats.errorAnalysis.distractionErrors;
       acc.totalInterpretation += stats.errorAnalysis.interpretationErrors;
-      acc.totalKnowledge += stats.errorAnalysis.knowledgeGaps;
+      acc.totalKnowledge += stats.errorAnalysis.knowledgeGapsErrors;
 
       return acc;
     },
@@ -170,7 +170,7 @@ export function MockExamItem({ mock }: { mock: MockExam }) {
                   </td>
                   {/* Conteúdo */}
                   <td className="p-2 font-bold text-orange-500">
-                    {stats.errorAnalysis.knowledgeGaps}
+                    {stats.errorAnalysis.knowledgeGapsErrors}
                   </td>
                 </tr>
               );
