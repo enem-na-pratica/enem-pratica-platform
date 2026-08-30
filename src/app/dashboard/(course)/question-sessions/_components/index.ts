c0c54983f7a0f93634ee3dcc48f5e2785ea5d2d4
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2026-08-30 19:05:24 -0300

    refactor(components): remove QuestionSessionForm export from index file

--- src/app/dashboard/(course)/question-sessions/_components/index.ts
+++ src/app/dashboard/(course)/question-sessions/_components/index.ts
@@ -1,4 +1,3 @@
 export { StatsSection } from './stats-section';
-export { QuestionSessionForm } from './_form';
 export { QuestionSessionItem } from './question-session-item';
 export { QuestionSessionListSection } from './question-session-list-section';
